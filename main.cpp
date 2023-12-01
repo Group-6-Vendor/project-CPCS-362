f0d533a7b3aac4d3668e25787d32c4a8cd4f6545
Lil-Dragon <55363094+Lil-Dragon@users.noreply.github.com>
2023-11-30 22:05:06 -0800

    Update main.cpp
    Reworked all of the main. Too messy to understand

--- main.cpp
+++ main.cpp
@@ -1,50 +1,31 @@
-// Dylan Drake
-
-
 // main.cpp
 
 #include <iostream>
-#include <cstdlib>                      // For system("clear") or system("cls") to clear the console
-#include "expire_tracker.cpp"
-#include "add_items.cpp"
-#include "delete_items.cpp"
-#include "clear_items.cpp"
+#include <cstring>
+#include <iomanip>
 
-//using namespace std           // not needed...for now
+#include "add_items.h"
+#include "delete_items.h"
+#include "clear_items.h"
+#include "expire_tracker.h"
+#include "purchase_item.h"
 
 
-// Function to handle expired items
-void handleExpiredItems(const std::string& machineID) {
-
-    std::vector<VendingMachineItem> expiringItems = expire_tracker(machineID);   // Call the expire_tracker() function
-
-    // Get the current date
-    auto now = std::chrono::system_clock::now();
-
-    std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
-
-    clearConsole();
-    
-    std::cout << "Expiring Items within 30 days:" << std::endl;
-    
-    for (const auto& item : expiringItems) {
-        std::cout << "Name: " << item.name << " | Location: " << item.location
-                  << " | Expiration Date: " << item.expirationDate << std::endl;
-    }
-
-    // Wait for user input before returning to the menu
-    std::cout << "\nPress Enter to return to the main menu...";
-
-    std::cin.ignore(); std::cin.get();    
+static void clearConsole() {
+#ifdef _WIN32
+    std::system("cls");
+#else
+    std::system("clear");
+#endif
 }
 
 
 int main() {
     int choice;
-    std::string machineID;  // Store vending machine #
+    std::string machineID;  // Store the vending machine number here
 
     do {
-        clearConsole();
+       // clearConsole();     // clear any previous output before displaying the menu
         // Display the menu
         std::cout << "Vending Machine Management System\n"
                   << "1. Enter Vending Machine ID Number\n"
@@ -52,7 +33,8 @@ int main() {
                   << "3. Delete Items\n"
                   << "4. Expired Checker\n"
                   << "5. Clear Expired List\n"
-                  << "6. Quit\n\n";
+                  << "6. Purchase Item\n"
+                  << "7. Quit\n\n";
 
         // If the vending machine number is not set, prompt the user to enter it
         if (machineID.empty()) {
@@ -63,43 +45,58 @@ int main() {
         std::cout << "Current Vending Machine ID: " << machineID << "\n\n";
 
         // Display additional menu options based on whether a vending machine number is set
+        // Consider additional methods of implementation here or QOL
         std::cout << "Enter your choice (";
-        if (!machineID.empty()) {
+        if (!machineID.empty() ) 
+        {
             std::cout << "2-6 or ";
         }
+
         std::cout << "1 to change vending machine): ";
+        // Stay on this line for user to clearly see choice...
+
 
         // Get user input for the menu choice
         std::cin >> choice;
 
+        
+    std::vector<VendingMachineItem> expiredItems = expire_tracker(machineID);
         // Perform actions based on the user's choice
         switch (choice) {
             case 1:
                 // Prompt the user to enter the vending machine number
                 std::cout << "Enter Vending Machine ID Number: ";
-                // User entering number for choice
                 std::cin >> machineID;
                 break;
             case 2:
                 addItems(machineID);
                 break;
             case 3:
-              deleteItems(machineID);
-                 break;
+                deleteItems(machineID);
+                break;
             case 4:
-              handleExpiredItems(machineID);
-              break;
+              expiredItems = expire_tracker(machineID);
+                // Process the expired items as needed
+                for (const auto& item : expiredItems) {
+                    std::cout << "Expired Item: " << item.name << " at " << item.location << " on " << item.expirationDate << "\n";
+                }
+                std::cout << "Press Enter to continue...";
+                std::cin.ignore();
+                std::cin.get();
+                break;
             case 5:
-               clearItems();
+                clearItems();
                 break;
             case 6:
+                purchaseItem(machineID);
+                break;
+            case 7:
                 std::cout << "Exiting the program. Goodbye!\n";
                 break;
-
-            default:        // If a choice is invalid, this will trigger below!
+            default:
                 std::cout << "Invalid choice. Please enter a number between 1 and 6.\n";
         }
-    } while (choice != 6);   // If "6", then we exit program
+    } while (choice != 7);
 
     return 0;
 }
