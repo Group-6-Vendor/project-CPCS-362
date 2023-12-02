3ff2e69d0d39808e45607f96e740a70b232ee29b
Debb1eL0w <126133834+Debb1eL0w@users.noreply.github.com>
2023-12-02 00:35:06 -0800

    consistent parameters

--- Location_identifier.cpp
+++ Location_identifier.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
-#include <string> 
+#include <string>
 
 class VendingMachine {
 private:
     std::string machineID;
     std::string location;
-    std::string Address;
-    std::string State;
+    std::string address;  // Changed from Address to address for consistency
+    std::string state;    // Changed from State to state for consistency
 
 public:
     // Constructor
     VendingMachine(const std::string& id, const std::string& loc, const std::string& adr, const std::string& st)
-        : machineID(id), location(loc), Address(adr), State(st) {}
+        : machineID(id), location(loc), address(adr), state(st) {}
 
     // Function to get and display location information
     void displayLocationInfo() const {
         std::cout << "Vending Machine ID: " << machineID << std::endl;
         std::cout << "Location: " << location << std::endl;
-        std::cout << "Address: " << Address << std::endl;
-        std::cout << "Location: " << State << std::endl;
+        std::cout << "Address: " << address << std::endl;
+        std::cout << "State: " << state << std::endl;  // Updated from Location to State
     }
-
 };
 
 int main() {
