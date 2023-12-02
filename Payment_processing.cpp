692a2eae4dfcdee69a627bd8c2a0483a0790e9c8
Debb1eL0w <126133834+Debb1eL0w@users.noreply.github.com>
2023-12-02 01:24:07 -0800

    Input Validation / Error Handling

--- Payment_processing.cpp
+++ Payment_processing.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <iomanip>
 
 // Function to insert money in cash and coins
-void insertMoney(float& customerPayment) {
+void insertMoney(float &customerPayment) {
     while (true) {
         try {
             float money;
@@ -12,9 +13,13 @@ void insertMoney(float& customerPayment) {
                 break;
             }
 
+            if (money < 0) {
+                throw std::invalid_argument("Amount cannot be negative.");
+            }
+
             customerPayment += money;
-        } catch (...) {
-            std::cout << "Invalid input. Please enter a valid amount." << std::endl;
+        } catch (const std::exception &e) {
+            std::cerr << "Invalid input: " << e.what() << std::endl;
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
@@ -22,31 +27,40 @@ void insertMoney(float& customerPayment) {
 }
 
 int main() {
-    // Get the total price of the selected item
-    float totalPrice;
-    std::cout << "Enter the total price: $";
-    std::cin >> totalPrice;
+    try {
+        // Get the total price of the selected item
+        float totalPrice;
+        std::cout << "Enter the total price: $";
+        std::cin >> totalPrice;
 
-    // Initialize variables for the customer's payment and change
-    float customerPayment = 0.0;
+        if (totalPrice < 0) {
+            throw std::invalid_argument("Total price cannot be negative.");
+        }
 
-    // Get the customer's payment in cash and coins
-    std::cout << "Insert cash and coins to pay for the item:" << std::endl;
-    insertMoney(customerPayment);
+        // Initialize variables for the customer's payment and change
+        float customerPayment = 0.0;
 
-    // Calculate the change and provide the receipt
-    while (customerPayment < totalPrice) {
-        std::cout << "Remaining balance: $" << std::fixed << std::setprecision(2) << totalPrice - customerPayment << std::endl;
-        std::cout << "Insufficient payment. Please provide more cash and coins." << std::endl;
+        // Get the customer's payment in cash and coins
+        std::cout << "Insert cash and coins to pay for the item:" << std::endl;
         insertMoney(customerPayment);
+
+        // Calculate the change and provide the receipt
+        while (customerPayment < totalPrice) {
+            std::cout << "Remaining balance: $" << std::fixed << std::setprecision(2) << totalPrice - customerPayment << std::endl;
+            std::cout << "Insufficient payment. Please provide more cash and coins." << std::endl;
+            insertMoney(customerPayment);
+        }
+
+        float change = customerPayment - totalPrice;
+
+        std::cout << "Thank you for your purchase!" << std::endl;
+        std::cout << "Total price: $" << std::fixed << std::setprecision(2) << totalPrice << std::endl;
+        std::cout << "Customer payment: $" << std::fixed << std::setprecision(2) << customerPayment << std::endl;
+        std::cout << "Change: $" << std::fixed << std::setprecision(2) << change << std::endl;
+
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
     }
-
-    float change = customerPayment - totalPrice;
-
-    std::cout << "Thank you for your purchase!" << std::endl;
-    std::cout << "Total price: $" << std::fixed << std::setprecision(2) << totalPrice << std::endl;
-    std::cout << "Customer payment: $" << std::fixed << std::setprecision(2) << customerPayment << std::endl;
-    std::cout << "Change: $" << std::fixed << std::setprecision(2) << change << std::endl;
 
     return 0;
 }
